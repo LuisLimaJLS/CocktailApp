5c4083c61301735ccabf9c618045942363b82895
luisl <llima@planerp.ec>
2023-10-22 22:41:06 -0500

    Add: create first activity

--- app/src/main/java/es/universidadviu/cocktailapp/CocktailListActivity.kt
+++ app/src/main/java/es/universidadviu/cocktailapp/CocktailListActivity.kt
@@ -4,33 +4,39 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
 
-class MainActivity : AppCompatActivity() {
-
+class CocktailListActivity : AppCompatActivity() {
 
     //Componentes
-    private lateinit var txtName: TextInputEditText
-    private lateinit var btnStart: Button
-
+    private lateinit var tvWelcome: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_cocktail_list)
+
         //Inicializacion de componentes
         initComponents()
         initListener()
+
+        //Recibimos el nombre
+        val name:String=intent.extras?.getString("name").orEmpty()
+        if(name.isNotEmpty()){
+            val welcome: String = getString(R.string.welcome_string,name)
+            tvWelcome.text = welcome
+        }
     }
+
     private fun initComponents(){
-        txtName = findViewById(R.id.txtNameIn)
-        btnStart = findViewById(R.id.btnStart)
+        tvWelcome = findViewById(R.id.tvWelcome)
     }
 
     private fun initListener(){
-        //Pasar a la lista de cocteles
-        btnStart.setOnClickListener{
+        //botones
+        /*btnStart.setOnClickListener{
             val intent = Intent(this,CocktailListActivity::class.java)
             intent.putExtra("name",txtName.text.toString())
             startActivity(intent)
-        }
+        }*/
     }
 }
