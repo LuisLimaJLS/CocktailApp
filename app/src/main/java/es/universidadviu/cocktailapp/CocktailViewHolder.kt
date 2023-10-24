2154a558b8c9b969e5a33219382e5ff3f6366c50
luisl <llima@planerp.ec>
2023-10-24 12:43:44 -0500

    Add: validations

--- app/src/main/java/es/universidadviu/cocktailapp/CocktailViewHolder.kt
+++ app/src/main/java/es/universidadviu/cocktailapp/CocktailViewHolder.kt
@@ -3,6 +3,7 @@ package es.universidadviu.cocktailapp
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import es.universidadviu.cocktailapp.models.Cocktail
@@ -11,9 +12,11 @@ class CocktailViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val txtName: TextView =view.findViewById(R.id.idDrink)
     private val txtCategory: TextView =view.findViewById(R.id.idCategory)
     private val ivImage: ImageView =view.findViewById(R.id.ivStrDrinkThumb)
+    private val divider: View = view.findViewById(R.id.divider)
     fun render(cocktail: Cocktail){
         Picasso.get().load(cocktail.strDrinkThumb).into(ivImage)
         txtName.text = cocktail.strDrink
         txtCategory.text = cocktail.strCategory
+        divider.setBackgroundColor(ContextCompat.getColor(divider.context,R.color.component_color))
     }
 }
--- app/src/main/java/es/universidadviu/cocktailapp/MainActivity.kt
+++ app/src/main/java/es/universidadviu/cocktailapp/MainActivity.kt
@@ -3,7 +3,10 @@ package es.universidadviu.cocktailapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -28,9 +31,29 @@ class MainActivity : AppCompatActivity() {
     private fun initListener(){
         //Pasar a la lista de cocteles
         btnStart.setOnClickListener{
+            startActivityList()
+        }
+        txtName.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if ((keyCode == KeyEvent.KEYCODE_ENTER) && (event.action == KeyEvent.ACTION_DOWN)) {
+                startActivityList()
+                return@OnKeyListener true
+            }
+            false
+        })
+    }
+
+    private fun startActivityList(){
+        if (!txtName.text.toString().isNullOrEmpty()){
             val intent = Intent(this,CocktailListActivity::class.java)
             intent.putExtra("name",txtName.text.toString())
             startActivity(intent)
+        } else{
+            showError()
         }
+
+    }
+
+    private fun showError(){
+        Toast.makeText(this, "Por favor ingrese su nombre", Toast.LENGTH_SHORT).show()
     }
 }
